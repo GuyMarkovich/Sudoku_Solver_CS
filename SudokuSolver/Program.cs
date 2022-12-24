51e90e99df0f71931cd335b73746f02ca18aec0c
GuyMarkovich <guymarkovich20@gmail.com>
2022-12-24 11:41:59 +0200

    Add project files.

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+static class constants
+{
+    //length of the side of the grid
+    public const double HEIGHT = 9;
+    //width of the grid
+    public const double WIDTH = 9;
+}
+
+class sudoku
+{
+    //height of a matrix which is part of the grid
+    double mat_height = Math.Sqrt(constants.HEIGHT);
+    //width of  a matrix which is part of the grid
+    double mat_width = Math.Sqrt(constants.HEIGHT);
+}
